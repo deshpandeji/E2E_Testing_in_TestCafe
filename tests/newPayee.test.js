08d529c5cca176276910cfebe7e2e10a99657151
Nagesh Deshpande <deshpandeji@users.noreply.github.com>
2020-04-16 21:26:56 +0530

    Helper Added

--- tests/newPayee.test.js
+++ tests/newPayee.test.js
@@ -1,11 +1,12 @@
 import { Selector } from 'testcafe'
+import { login } from '../helper'
 
 //prettier-ignore
 fixture`New Payee test`
     .page`http://zero.webappsecurity.com/index.html`
 
-    test
-    .before(async t =>{
+test.before(async t => {
+  /*
         const signInButton = Selector("#signin_button")
         const loginName = Selector("#user_login")
         const password = Selector("#user_password")
@@ -19,27 +20,25 @@ fixture`New Payee test`
         await t.typeText(loginName, "username", {paste:true})
         await t.typeText(password, "password", {paste: true})
         await t.click(submitButton)
-    })
-    ("User Add a new Payee", async t => {
-        const payBillTab = Selector("#pay_bills_tab")
-        const addNewPayeeTab = Selector("a").withText("Add New Payee")
-        const payeeName = Selector("#np_new_payee_name")
-        const payeeAddress = Selector("#np_new_payee_address")
-        const payeeAccount = Selector("#np_new_payee_account")
-        const payeeDetails = Selector("#np_new_payee_details")
-        const addButton = Selector("input").withAttribute("value", "Add")
-        const addMessage = Selector("#alert_content")
+        */
+  await login('username', 'password')
+})('User Add a new Payee', async t => {
+  const payBillTab = Selector('#pay_bills_tab')
+  const addNewPayeeTab = Selector('a').withText('Add New Payee')
+  const payeeName = Selector('#np_new_payee_name')
+  const payeeAddress = Selector('#np_new_payee_address')
+  const payeeAccount = Selector('#np_new_payee_account')
+  const payeeDetails = Selector('#np_new_payee_details')
+  const addButton = Selector('input').withAttribute('value', 'Add')
+  const addMessage = Selector('#alert_content')
 
-        await t.click(payBillTab)
-        await t.click(addNewPayeeTab)
-        await t.typeText(payeeName, "Dinesh Diwan", {paste:true})
-        await t.typeText(payeeAddress, "WAkad, Pune", {paste:true})
-        await t.typeText(payeeAccount, "1234567890", {paste:true})
-        await t.typeText(payeeDetails, "Current Account", {paste:true})
-        await t.click(addButton)
-        
+  await t.click(payBillTab)
+  await t.click(addNewPayeeTab)
+  await t.typeText(payeeName, 'Dinesh Diwan', { paste: true })
+  await t.typeText(payeeAddress, 'WAkad, Pune', { paste: true })
+  await t.typeText(payeeAccount, '1234567890', { paste: true })
+  await t.typeText(payeeDetails, 'Current Account', { paste: true })
+  await t.click(addButton)
 
-        await t.expect(addMessage.innerText).contains("Dinesh Diwan")
-
-
-    })
+  await t.expect(addMessage.innerText).contains('Dinesh Diwan')
+})
--- tests/validLogin.test.js
+++ tests/validLogin.test.js
@@ -4,30 +4,28 @@ import { Selector } from 'testcafe'
 fixture`Valid login`
     .page`http://zero.webappsecurity.com/index.html`
 
-    test("User Login with valid credentials", async t => {
-        const signInButton = Selector("#signin_button")
-        const loginName = Selector("#user_login")
-        const password = Selector("#user_password")
-        const submitButton = Selector(".btn-primary")
-        const loginForm = Selector("#login_form")
-        const accountSummery = Selector("#account_summary_tab")
-        const userIcon = Selector(".icon-user")
-        const logOut = Selector("#logout_link")
-        //const homeMenu = Selector("#homeMenu")
+test('User Login with valid credentials', async t => {
+  const signInButton = Selector('#signin_button')
+  const loginName = Selector('#user_login')
+  const password = Selector('#user_password')
+  const submitButton = Selector('.btn-primary')
+  const loginForm = Selector('#login_form')
+  const accountSummery = Selector('#account_summary_tab')
+  const userIcon = Selector('.icon-user')
+  const logOut = Selector('#logout_link')
+  //const homeMenu = Selector("#homeMenu")
 
-        await t.click(signInButton)
-        await t.typeText(loginName, "username", {paste:true})
-        await t.typeText(password, "password", {paste: true})
-        await t.click(submitButton)
-        
+  await t.click(signInButton)
+  await t.typeText(loginName, 'username', { paste: true })
+  await t.typeText(password, 'password', { paste: true })
+  await t.click(submitButton)
 
-        await t.expect(accountSummery.exists).ok()
-        await t.expect(loginForm.exists).notOk()
+  await t.expect(accountSummery.exists).ok()
+  await t.expect(loginForm.exists).notOk()
 
-        await t.click(userIcon)
-        await t.click(logOut)
+  await t.click(userIcon)
+  await t.click(logOut)
 
-        await t.expect(accountSummery.exists).notOk()
-        await t.expect(signInButton.exists).ok()
-
-    })
+  await t.expect(accountSummery.exists).notOk()
+  await t.expect(signInButton.exists).ok()
+})
