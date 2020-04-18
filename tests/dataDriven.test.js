7e0e620b0777d742eadd2907863ff2f026e7bc28
deshpandeji <deshpandeji@users.noreply.github.com>
2020-04-18 19:48:13 +0530

    Data Driven Test Added

---
+++ tests/dataDriven.test.js
@@ -0,0 +1,19 @@
+import { Selector } from 'testcafe'
+import Navbar from '../pageObjects/componants/navbar'
+
+const navbar = new Navbar()
+const dataSet = require('../data/data.json')
+
+fixture`Data Driven Test using TestCafe`
+  .page`http://zero.webappsecurity.com/index.html`
+
+dataSet.forEach(data => {
+  test(`Test Search with Data Driven`, async t => {
+    //const searchTextbox = Selector('#searchTerm')
+    await t.typeText(navbar.searchTextbox, data.SearchText, {
+      paste: true,
+      replace: true,
+    })
+    await t.pressKey('enter')
+  })
+})
